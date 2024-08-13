20126e9173be1dabc9df76e50267de0667aa590c
Muhammad Rasyad Caesarardhi <mrcrasyad26@gmail.com>
2024-08-13 14:49:11 +0700

    Initial Commit

---
+++ PukPuk/PukPukApp.swift
@@ -0,0 +1,17 @@
+//
+//  PukPukApp.swift
+//  PukPuk
+//
+//  Created by Muhammad Rasyad Caesarardhi on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PukPukApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

9fd1baccbab7f1c9912750624bd5997c5cfa2b50
Muhammad Rasyad Caesarardhi <mrcrasyad26@gmail.com>
2024-08-13 15:13:03 +0700

    initial commit

---
+++ PukPuk/DependencyInjection.swift
@@ -0,0 +1,46 @@
+//
+//  DependencyInjection.swift
+//  PukPuk
+//
+//  Created by Muhammad Rasyad Caearardhi on 13/08/24.
+//
+
+import Foundation
+import SwiftData
+
+// Grouping UseCase ke dalam satu fungsi
+class DependencyInjection: ObservableObject {
+    static let shared = DependencyInjection()
+
+    private init() {}
+}
+
+//// Singleton instance
+// private var createPlanViewModelInstance: CreateEditPlanViewModel?
+//
+//// MARK: IMPLEMENTATION
+//
+// lazy var planLocalDataSource = PlanLocalDataSource(modelContext: modelContext!)
+// lazy var aqiDataSource = AQIRemoteDataSource()
+//
+// lazy var planRepository = PlanRepository(planLocalDataSource: planLocalDataSource)
+// lazy var aqiRepository = AQIRepository(AQIRemoteDataSource: aqiDataSource)
+//
+//// MARK: IMPLEMENTATION USE CASES
+//
+// lazy var getPlanPreviewUseCase = PlanUseCases(planRepository: planRepository, AQIRepository: aqiRepository)
+// lazy var refreshPageViewUseCase = RefreshHomeViewUseCase(planRepository: planRepository)
+//
+//// MARK: TESTING
+//
+// lazy var dummyPlanRepository = DummyPlanRepository(dummyPlans: dummyPlans)
+// lazy var dummyGetAllPlansPreviewUseCase = PlanUseCases(planRepository: dummyPlanRepository, AQIRepository: aqiRepository)
+// lazy var dummyRefreshHomeViewUseCase = RefreshHomeViewUseCase(planRepository: DummyPlanRepository(dummyPlans: dummyPlans))
+//
+//// MARK: FUNCTION
+//
+// func homeViewModel() -> HomeViewModel {
+//    HomeViewModel(
+//        getAllPlansUseCase: getPlanPreviewUseCase
+//    )
+// }
--- PukPuk/PukPukApp.swift
+++ PukPuk/PukPukApp.swift
@@ -12,6 +12,9 @@ struct PukPukApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+
+//            HomeView()
+//                .environmentObject(DependencyInjection.shared.homeViewModel())
         }
     }
 }

d60ffb90c3e9823d11e95130015dd5cd0b70eeec
senakristiawan! <senakristiawan1@gmail.com>
2024-08-15 09:07:31 +0700

    Result Storyboard

---
+++ PukPuk/Modules/HomePage/Scene/View/ResultVC.swift
@@ -0,0 +1,29 @@
+//
+//  ResultVCViewController.swift
+//  PukPuk
+//
+//  Created by Sena Kristiawan on 14/08/24.
+//
+
+import UIKit
+
+class ResultVC: UIViewController {
+
+    @IBOutlet var percentageLabel: UILabel!
+    @IBOutlet var causeLabel: UILabel!
+    @IBOutlet var descLabel: UILabel!
+    @IBOutlet var xReviewButton: UIButton!
+    @IBOutlet var checkReviewButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func xAction(_ sender: Any) {
+    }
+    
+    @IBAction func checkAction(_ sender: Any) {
+    }
+}
